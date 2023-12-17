e12545b41320151f007d3b32306c3dcd2a60bf4f
Serezha <volos11223344@yandex.ru>
2023-12-17 14:53:15 +0300

    рабочая карта

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+const startLong = 59.55321;
+const endLong = 60.113213;
+const startLat = 30.192131;
+const endLat = 30.593123;
+const data = [];
+// const data = [];
+
+function createCoordinates() {
+	const coor = [];
+	function getRandomNumber(min, max) {
+		let res = Math.random() * (max - min) + min;
+		const fixedNum = res.toFixed(4);
+		coor.push(Number(fixedNum));
+	}
+	getRandomNumber(startLong, endLong);
+	getRandomNumber(startLat, endLat);
+	data.push(coor);
+}
+
+// for (let i = 0; i < 10; i++) {
+// 	createCoordinates();
+// 	console.log(data);
+// }
+
 ymaps.ready(function () {
 	var map = new ymaps.Map('map', {
 		center: [59.9386, 30.3141],
@@ -6,15 +30,12 @@ ymaps.ready(function () {
 	});
 
 	ymaps.modules.require(['Heatmap'], function (Heatmap) {
-		var data = [
-				[59.9357, 30.3138],
-				[59.9347, 30.3146],
-				[59.9357, 30.3138],
-				[59.9347, 30.3146],
-				[59.9357, 30.3138],
-				[59.9347, 30.3146],
-			],
-			heatmap = new Heatmap(data);
+		for (let i = 0; i < 500; i++) {
+			createCoordinates();
+			console.log(data);
+		}
+
+		heatmap = new Heatmap(data);
 		heatmap.setMap(map);
 		map.geoObjects.add(heatmap);
 	});
